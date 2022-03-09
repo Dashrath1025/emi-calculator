3d345f34e6eb8e127b49383eec805a4ac139e4c5
Dashrath1025 <dashrathkanzariya1025@gmail.com>
2022-03-09 13:38:31 +0530

    fixed validation

--- tests/emi.test.js
+++ tests/emi.test.js
@@ -6,11 +6,10 @@ test('Loan is defined', () => {
 
 test('Should throw an error on negative interest rate', () => {
   expect(() => EMI.Loan(10000, -1, 10)).toThrowError('wrong parameters: 10000 -1 10')
-});
+})
 
 test('Should calculate correct EMI installments', () => {
   const loan = EMI.Loan(10000, 12, 10)
-
 
   expect(loan).toBeDefined()
   expect(loan.installments.length).toBe(12)
